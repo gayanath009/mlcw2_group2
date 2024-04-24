26347260aeea6c61c100d0a35c4681ca152ee6f0
Gayanath Perera <48854057+gayanath009@users.noreply.github.com>
2024-04-24 14:18:46 +0000

    Data Size Added

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import requests
-from flask import Flask,render_template, request, jsonify
+from flask import Flask,render_template, jsonify, request
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-#import arff as arf # using 'arff' instead of 'arff2pandas' is not avaiable to install over pip
 from keras.models import load_model
 from keras import Sequential
 from keras import layers
@@ -20,35 +19,39 @@ app = Flask(__name__)
 def index(): 
    return render_template ("index.html")
 
-
 @app.route('/predict', methods = ["POST"])
-def predict():
+def predict():   
+    data = request.get_json()
+    frctn = data['frctn'] 
+    
+    api_url = f"https://mlcw2-api.onrender.com/GetData?Fraction={frctn}"
+     
+    result = ""           
+    response = requests.get(api_url)
+    dataChunk = pd.DataFrame(response.json())
+    dataChunk.dropna() # drop null values 
+    dataChunk.drop_duplicates() # drop duplicates 
+    dataChunk.iloc[:, -1] = dataChunk.iloc[:, -1].apply(lambda x: 0 if x != 1 else x)
 
-    api_url = "https://mlcw2-api.onrender.com/GetData?Fraction=0.02"    
-    while True:
-        message = ""   
+    anomalies, y_preds, y_pred_classes, accuracy, f1, precision = anomalyV2(dataChunk)
 
-        response = requests.get(api_url)
-        dataChunk = pd.DataFrame(response.json())
-        dataChunk.dropna() # drop null values 
-        dataChunk.drop_duplicates() # drop duplicates 
-        dataChunk.iloc[:, -1] = dataChunk.iloc[:, -1].apply(lambda x: 0 if x != 1 else x)
-
-        anomalies, y_preds, y_pred_classes, accuracy, f1, precision = anomalyV2(dataChunk)
-
-        #print("Anomaly ECGs :", np.sum(anomalies.numpy())) 
+        #print("Anomaly ECGs :", np.sum(anomalies.numpy()))
         #print("Normal ECGs :", len(anomalies) - np.sum(anomalies.numpy()))
         #print ("Accuracy : " , accuracy)
         #print ("F1 : " , f1)
         #print ("Precision : " , precision)  
-        anamolies = np.sum(anomalies.numpy()) 
-        normal = len(anomalies) - np.sum(anomalies.numpy())
+    anamolies = np.sum(anomalies.numpy()) 
+    normal = len(anomalies) - np.sum(anomalies.numpy())
+       
+    if anamolies > 0 :
+        result = f'Anomalies Detected - Anomaly ECGs :  {str(anamolies)} | Normal ECGs : {str(normal)}'
+        color = 'red'
+    else :         
+        result = "No Anomalies Found"  
+        color = 'green'
+    
+    return jsonify({'message': result, 'color': color})
 
-        if anamolies > 0 :
-           message = f'Anomalies Detected - Anomaly ECGs :  {str(anamolies)} | Normal ECGs : {str(normal)}'
-        else :         
-           message = "No Anomalies Found"  
-        return render_template('index.html',  msg =message)  
 
 # Setting other anomaly classes also to 0
 def ecgclass(classId):
@@ -56,25 +59,25 @@ def ecgclass(classId):
        return 0
    else: 
        return 1
-
+   
 
 # Model Anomaly
 def anomalyV2(dataChunk):    
 
     X_test = dataChunk.iloc[:,:-1].values
     y_test = dataChunk.iloc[:,-1].values 
-
+            
     # Normalize the data
     min_val = tf.reduce_min(X_test)
     max_val = tf.reduce_max(X_test)
     X_test = (X_test - min_val) / (max_val - min_val)
-
+        
     # Generate reconstruction from the model
     X_test_recon = new_Anomaly(X_test)
-
+      
     # Calculate loss
     loss = tf.keras.losses.mae(X_test_recon, X_test)
-
+    
     # Apply the threshold to the loss to get the predictions
     #threshold = 0.04
     threshold = np.mean(loss) + np.std(loss) 
@@ -83,7 +86,7 @@ def anomalyV2(dataChunk):
 
     #anomalies
     anomalies= tf.math.greater(loss, threshold)
-
+    
     # Convert predictions to a list
     y_preds = preds.numpy().tolist()
     y_pred_classes = np.round(preds).astype(int)  # assuming a binary classification
@@ -91,46 +94,46 @@ def anomalyV2(dataChunk):
     accuracy = accuracy_score(y_test, y_pred_classes)
     f1 = f1_score(y_test, y_pred_classes)
     precision = precision_score(y_test, y_pred_classes)
-
+  
     return anomalies, y_preds, y_pred_classes, accuracy, f1, precision
 
 
-def anomalyV3(dataChunk):    
+# def anomalyV3(dataChunk):    
 
-    X_test = dataChunk.iloc[:,:-1].values
-    y_test = dataChunk.iloc[:,-1].values 
+#     X_test = dataChunk.iloc[:,:-1].values
+#     y_test = dataChunk.iloc[:,-1].values 
+            
+#     # Normalize the data
+#     min_val = tf.reduce_min(X_test)
+#     max_val = tf.reduce_max(X_test)
+#     X_test = (X_test - min_val) / (max_val - min_val)
+        
+#     # Generate reconstruction from the model
+#     X_test_recon = new_Anomaly(X_test)
+      
+#     # Calculate loss
+#     #loss = tf.keras.losses.mae(X_test_recon, X_test)
 
-    # Normalize the data
-    min_val = tf.reduce_min(X_test)
-    max_val = tf.reduce_max(X_test)
-    X_test = (X_test - min_val) / (max_val - min_val)
+#     mse = np.mean(np.power(X_test - X_test_recon, 2), axis=1)
+    
+#     # Apply the threshold to the loss to get the predictions
+#     threshold = 0.6
+#     #threshold = np.mean(loss) + np.std(loss) 
+#     #preds = tf.math.less(loss, threshold)
 
-    # Generate reconstruction from the model
-    X_test_recon = new_Anomaly(X_test)
+#     #anomalies
+#     #anomalies= tf.math.greater(loss, threshold)
+#     anomalies = mse > threshold
+    
+#     # Convert predictions to a list
+#     y_preds =  np.argmax(y_test, axis=1)
+#     y_pred_classes = anomalies.astype(int)  # assuming a binary classification
 
-    # Calculate loss
-    #loss = tf.keras.losses.mae(X_test_recon, X_test)
-
-    mse = np.mean(np.power(X_test - X_test_recon, 2), axis=1)
-
-    # Apply the threshold to the loss to get the predictions
-    threshold = 0.6
-    #threshold = np.mean(loss) + np.std(loss) 
-    #preds = tf.math.less(loss, threshold)
-
-    #anomalies
-    #anomalies= tf.math.greater(loss, threshold)
-    anomalies = mse > threshold
-
-    # Convert predictions to a list
-    y_preds =  np.argmax(y_test, axis=1)
-    y_pred_classes = anomalies.astype(int)  # assuming a binary classification
-
-    accuracy = accuracy_score(y_test, y_pred_classes)
-    f1 = f1_score(y_test, y_pred_classes)
-    precision = precision_score(y_test, y_pred_classes)
-
-    return anomalies, y_preds, y_pred_classes, accuracy, f1, precision
+#     accuracy = accuracy_score(y_test, y_pred_classes)
+#     f1 = f1_score(y_test, y_pred_classes)
+#     precision = precision_score(y_test, y_pred_classes)
+  
+#     return anomalies, y_preds, y_pred_classes, accuracy, f1, precision
 
 
 
@@ -153,7 +156,7 @@ class DetectAnomaly(Model):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
         return decoded    
-
+    
 custom_objects = {'DetectAnomaly': DetectAnomaly}
 new_Anomaly = load_model("models/anomaly.keras", custom_objects=custom_objects)
 
